9d2a8372ac9b627c5fc5133c7ca6eae13e4163fe
danielbatres <danielbatresuser@gmail.com>
2023-02-13 15:41:19 -0600

    Setting entity framework with context

--- ef-project/TasksContext.cs
+++ ef-project/TasksContext.cs
@@ -6,4 +6,6 @@ namespace ef_project;
 public class TasksContext : DbContext {
   public DbSet<Category> Categories { get; set; }
   public DbSet<Task> Tasks { get; set; }
+
+  public TasksContext(DbContextOptions<TasksContext> options) : base(options) { }
 }
