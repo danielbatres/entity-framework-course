4b1d6c618aed2a02092d351c5044966b53b48019
danielbatres <danielbatresuser@gmail.com>
2023-02-13 17:01:53 -0600

    In-memory database creation and configuration

--- ef-project/Program.cs
+++ ef-project/Program.cs
@@ -1,6 +1,18 @@
+using ef_project;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TasksContext>(otp => otp.UseInMemoryDatabase("TasksDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconnection", async ([FromServices] TasksContext dbContext) => {
+  dbContext.Database.EnsureCreated();
+  return Results.Ok($"Database in memory: {dbContext.Database.IsInMemory()}");
+});
 
 app.Run();
