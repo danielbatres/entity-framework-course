098eb7a693f76470fb727fd43a9e8e01438fe36e
danielbatres <danielbatresuser@gmail.com>
2023-02-13 16:36:32 -0600

    Use of data annotations to designate properties in database

--- ef-project/Models/Task.cs
+++ ef-project/Models/Task.cs
@@ -1,13 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ef_project.Models;
 
 public class Tasks {
+  [Key]
   public Guid TaskId { get; set; }
+  [ForeignKey("CategoryId")]
   public Guid CategoryId { get; set; }
+  [Required]
+  [MaxLength(200)]
   public string Title { get; set; }
   public string Description { get; set; }
   public Priority TaskPriority { get; set; }
   public DateTime CreationDateTime { get; set; }
   public virtual Category Category { get; set; }
+  [NotMapped]
+  public string Summary { get; set; }
 }
 
 public enum Priority {
