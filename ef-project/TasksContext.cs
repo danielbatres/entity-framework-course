29bba795d690eb192472b306c851a8f535adef7c
danielbatres <danielbatresuser@gmail.com>
2023-04-22 20:19:44 -0600

    Model configuration using Fluent API

--- ef-project/TasksContext.cs
+++ ef-project/TasksContext.cs
@@ -9,4 +9,13 @@ public class TasksContext : DbContext {
   public DbSet<Task> Tasks { get; set; }
 
   public TasksContext(DbContextOptions<TasksContext> options) : base(options) { }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder) {
+    modelBuilder.Entity<Category>(category => {
+      category.ToTable("Category");
+      category.HasKey(key => key.CategoryId);
+      category.Property(p => p.Name).IsRequired().HasMaxLength(150);
+      category.Property(p => p.Description);
+    });
+  }
 }
