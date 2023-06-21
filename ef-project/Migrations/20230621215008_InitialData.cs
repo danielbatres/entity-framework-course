b99de09e69a492e3e60a0988cb357baba397c1f2
danielbatres <danielbatresuser@gmail.com>
2023-06-21 15:53:53 -0600

    Add seed data

---
+++ ef-project/Migrations/20230621215008_InitialData.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ef_project.Migrations
+{
+    public partial class InitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Weigth",
+                table: "Category",
+                newName: "Weight");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Task",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Category",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "CategoryId", "Description", "Name", "Weight" },
+                values: new object[] { new Guid("824d3c0a-900f-4069-a80f-ade59940e020"), null, "Pending activities", 20 });
+
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "CategoryId", "Description", "Name", "Weight" },
+                values: new object[] { new Guid("824d3c0a-900f-4069-a80f-ade59940e022"), null, "Personal activities", 50 });
+
+            migrationBuilder.InsertData(
+                table: "Task",
+                columns: new[] { "TaskId", "CategoryId", "CreationDateTime", "Description", "TaskPriority", "Title" },
+                values: new object[] { new Guid("0bf7dc7e-930c-45b4-b957-d9b6e9f5a175"), new Guid("824d3c0a-900f-4069-a80f-ade59940e022"), new DateTime(2023, 6, 21, 15, 50, 8, 605, DateTimeKind.Local).AddTicks(8355), null, 0, "Finish watching movie on netflix" });
+
+            migrationBuilder.InsertData(
+                table: "Task",
+                columns: new[] { "TaskId", "CategoryId", "CreationDateTime", "Description", "TaskPriority", "Title" },
+                values: new object[] { new Guid("4da3fc64-ee46-4654-9387-f2ea2fe4116b"), new Guid("824d3c0a-900f-4069-a80f-ade59940e020"), new DateTime(2023, 6, 21, 15, 50, 8, 605, DateTimeKind.Local).AddTicks(8302), null, 1, "Payment of public services" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Task",
+                keyColumn: "TaskId",
+                keyValue: new Guid("0bf7dc7e-930c-45b4-b957-d9b6e9f5a175"));
+
+            migrationBuilder.DeleteData(
+                table: "Task",
+                keyColumn: "TaskId",
+                keyValue: new Guid("4da3fc64-ee46-4654-9387-f2ea2fe4116b"));
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "CategoryId",
+                keyValue: new Guid("824d3c0a-900f-4069-a80f-ade59940e020"));
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "CategoryId",
+                keyValue: new Guid("824d3c0a-900f-4069-a80f-ade59940e022"));
+
+            migrationBuilder.RenameColumn(
+                name: "Weight",
+                table: "Category",
+                newName: "Weigth");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Task",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Category",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- ef-project/TasksContext.cs
+++ ef-project/TasksContext.cs
@@ -11,12 +11,47 @@ public class TasksContext : DbContext {
   public TasksContext(DbContextOptions<TasksContext> options) : base(options) { }
 
   protected override void OnModelCreating(ModelBuilder modelBuilder) {
+    List<Category> CategoriesInit = new List<Category>();
+
+    CategoriesInit.Add(new Category() {
+      CategoryId = Guid.Parse("824d3c0a-900f-4069-a80f-ade59940e020"),
+      Name = "Pending activities",
+      Weight = 20
+    });
+
+    CategoriesInit.Add(new Category()
+    {
+      CategoryId = Guid.Parse("824d3c0a-900f-4069-a80f-ade59940e022"),
+      Name = "Personal activities",
+      Weight = 50
+    });
+    
     modelBuilder.Entity<Category>(category => {
       category.ToTable("Category");
       category.HasKey(p => p.CategoryId);
       category.Property(p => p.Name).IsRequired().HasMaxLength(150);
-      category.Property(p => p.Description);
+      category.Property(p => p.Description).IsRequired(false);
       category.Property(p => p.Weight);
+      category.HasData(CategoriesInit);
+    });
+
+    List<Task> TaskInit = new List<Task>();
+
+    TaskInit.Add(new Task() {
+      TaskId = Guid.Parse("4da3fc64-ee46-4654-9387-f2ea2fe4116b"),
+      CategoryId = Guid.Parse("824d3c0a-900f-4069-a80f-ade59940e020"),
+      TaskPriority = Priority.Mid,
+      Title = "Payment of public services",
+      CreationDateTime = DateTime.Now
+    });
+
+    TaskInit.Add(new Task()
+    {
+      TaskId = Guid.Parse("0bf7dc7e-930c-45b4-b957-d9b6e9f5a175"),
+      CategoryId = Guid.Parse("824d3c0a-900f-4069-a80f-ade59940e022"),
+      TaskPriority = Priority.Low,
+      Title = "Finish watching movie on netflix",
+      CreationDateTime = DateTime.Now
     });
 
     modelBuilder.Entity<Task>(task => {
@@ -24,10 +59,11 @@ public class TasksContext : DbContext {
       task.HasKey(p => p.TaskId);
       task.HasOne(p => p.Category).WithMany(p => p.Tasks).HasForeignKey(p => p.CategoryId);
       task.Property(p => p.Title).IsRequired().HasMaxLength(200);
-      task.Property(p => p.Description);
+      task.Property(p => p.Description).IsRequired(false);
       task.Property(p => p.TaskPriority);
       task.Property(p => p.CreationDateTime);
       task.Ignore(p => p.Summary);
+      task.HasData(TaskInit);
     });
   }
 }
